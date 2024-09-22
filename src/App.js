a63697e301b3ac9032f4573c523db5b1bef5e0bb
KanadKJ <jadhav.kanadkj@gmail.com>
2024-09-22 23:50:14 +0530

    R3F setup improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+// react imports
 import React from "react";
-
+// R3F imports
+import { Canvas } from "@react-three/fiber";
+// Components Import
+import Experience from "./Components/Experience";
+//css imports
+import "./App.css";
 export default function App() {
-  return <div>d</div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Canvas>
+        <Experience />
+      </Canvas>
+    </div>
+  );
 }
